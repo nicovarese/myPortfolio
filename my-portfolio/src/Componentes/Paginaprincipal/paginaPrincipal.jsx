835e265dcf6544d9ad33703b2e877aeb286d6059
santiVarese <varesesanti@gmail.com>
2022-10-05 18:28:04 -0300

    nicosalame

--- my-portfolio/src/Componentes/Paginaprincipal/paginaPrincipal.jsx
+++ my-portfolio/src/Componentes/Paginaprincipal/paginaPrincipal.jsx
@@ -4,8 +4,18 @@ import { Link } from "react-router-dom";
 import perfil from "../../Materiales/Perfil.JPG";
 
 
+
 function paginaPrincipal() {
   return <div className="contenedor">
+   <ul class="menu">
+  <li title="home"><a href="#" class="home">home</a></li>
+  <li title="search"><a href="#" class="search">search</a></li>
+  <li title="pencil"><a href="#" class="pencil">pencil</a></li>
+  <li title="about"><a href="#" class="active about">about</a></li>
+  <li title="archive"><a href="#" class="archive">archive</a></li>
+  <li title="contact"><a href="#" class="contact">contact</a></li>
+</ul>
+
 <div class="grid">
   <div class="grid__title">
     <h2>WELCOME !</h2>
@@ -27,10 +37,11 @@ function paginaPrincipal() {
     alt="Perfil"
   />
 </div>
-  <h5>klsgjksdg</h5>
-  <h5>klsgjksdg</h5>
-  <h5>klsgjksdg</h5>
-  <h5>klsgjksdg</h5>
+  <h5>Name: Santiago</h5>
+  <h5>Surname: Varese</h5>
+  <h5>Age: 23</h5>
+  <h5>Country: Uruguay</h5>
+  <h5>City: Montevideo</h5>
 </div>
 </div>
 </div>
