1ccb7f8853624d546e930b4861a8ac5cafedba55
nico_varese <nicolasvaresee@gmail.com>
2022-10-18 17:47:53 -0300

    segunda commit

--- my-portfolio/src/Componentes/About/About.jsx
+++ my-portfolio/src/Componentes/About/About.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./About.css";
 import { Link } from "react-router-dom";
 import NavBar from "../../NavBar/NavBar";
+import html5 from "../../Materiales/HTML5.png";
+import css from "../../Materiales/CSS.png";
+import javascript from "../../Materiales/javascript.png";
+import reactjs from "../../Materiales/react.png";
+import postgresql from "../../Materiales/PostgreSQL.png";
+import nodejs from "../../Materiales/nodeJS.png";
 
 function About() {
   return (
@@ -24,6 +30,38 @@ function About() {
           many desire to grow and learn, I am focused and that makes me clear
           what I want my future to be in the IT area.
         </p>
+      </div>
+      <div className="lenguajesBootcamp">
+        <p className="listaLenguajes">
+          During my time at the bootcamp I learned various languages ​​such as :
+        </p>
+
+        <ol className="centrarLista">
+          <div className="logoLenguajes">
+            <img src={html5} alt="html5" />
+            <ul>HTML</ul>
+          </div>
+          <div className="logoLenguajes">
+            <img className="logoHtml5" src={css} alt="css" />
+            <ul>CSS</ul>
+          </div>
+          <div className="logoLenguajes">
+            <img className="logoHtml5" src={javascript} alt="javascript" />
+            <ul>JAVASCRIPT</ul>
+          </div>
+          <div className="logoLenguajes">
+            <img className="logoHtml5" src={reactjs} alt="reactjs" />
+            <ul>REACTJS</ul>
+          </div>
+          <div className="logoLenguajes">
+            <img className="logoHtml5" src={postgresql} alt="postgreSQL" />
+            <ul>POSTGRESQL</ul>
+          </div>
+          <div className="logoLenguajes">
+            <img className="logoHtml5" src={nodejs} alt="nodejs" />
+            <ul>NODEJS</ul>
+          </div>
+        </ol>
       </div>
     </div>
   );
